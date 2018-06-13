830d3a545267fc909b9596e87dffd0521293a12e
surajpletratech <34180713+surajpletratech@users.noreply.github.com>
2018-06-13 21:24:03 +0530

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,13 @@ import {MatInputModule} from '@angular/material/input';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabase, AngularFireDatabaseModule } from 'angularfire2/database';
+import {environment} from '../environments/environment';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { HomeComponent } from './home/home.component';
@@ -11,6 +18,8 @@ import { ForgetpasswordComponent } from './forgetpassword/forgetpassword.compone
 import {FormsModule,FormControl, Validators, ReactiveFormsModule,FormGroup,FormBuilder} from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import {Routes, RouterModule } from '@angular/router'
+import {MatCardModule} from '@angular/material/card';
+import {DataService} from '../app/data-service.service';
 const appRoutes: Routes = [ 
   {
     path: '',
@@ -18,6 +27,10 @@ const appRoutes: Routes = [
   },
   { path: 'home', 
     component: HomeComponent
+   },
+   {
+     path:'signup',
+     component: SignupComponent
    }
   ];
 @NgModule({
@@ -36,10 +49,18 @@ const appRoutes: Routes = [
     MatButtonModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    MatCardModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
+    AngularFireStorageModule, // imports firebase/storage only needed for storage features
+    AngularFireAuthModule,
+    AngularFireDatabaseModule
   ],
 
   providers: [],   
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+
 })
 export class AppModule { }
--- src/app/data-service.service.ts
+++ src/app/data-service.service.ts
@@ -1,9 +1,89 @@
 import { Injectable } from '@angular/core';
-
+import { AngularFirestore, AngularFirestoreDocument, AngularFirestoreCollection } from 'angularfire2/firestore';
+import {environment} from '../environments/environment';
+import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import {User} from '../app/datamodel/datamodel'; 
+import * as firebase from 'firebase';
 @Injectable({
   providedIn: 'root'
 })
-export class DataServiceService {
+export class DataService {
+  user:any;
+ private userCollection:AngularFirestoreCollection<User>
+  constructor(private afs:AngularFirestore) { 
 
-  constructor() { }
-}
+    this.userCollection = afs.collection<User>("userprofile");
+    this.user = firebase.auth().currentUser;
+  }
+
+  getUserCollection(): AngularFirestoreCollection<User>{
+    return this.userCollection
+  }
+
+  getUser(id:string): User{
+    let name:string;
+    let mobile:string;
+
+    this.userCollection.doc(id).ref.get().then(doc=>{
+      if(doc.exists){
+        console.log('Document Exist',doc.data());
+        name = doc.data().name;
+        mobile = doc.data().mobile;
+      }
+      else{
+        console.log("No Such Document");
+      }
+    }).catch(err=>{
+      console.log("No Such Document");
+    });
+
+    let user:User ={
+      uid:id,
+      username: name,
+      mobile:mobile,
+      email:"",
+      password:""
+    }
+    console.log('auth user',user)
+    return user
+  }
+
+  signupUser(name: string,email: string,
+    password: string,
+    pmobile: string) {
+
+      console.log(name+'/'+email+''+'/'+password+'/'+'/'+pmobile);
+      
+    return firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
+        .then(newUser => {
+            console.log(newUser);
+            let data: User = {
+                uid: newUser.uid,
+                username: name,
+                email: email,
+                mobile: pmobile,
+                password:password
+            }
+            this.userCollection.doc(newUser.uid).set(data);
+            this.user = data;
+            return data;
+        })
+        .catch(error => {
+            console.error(error);
+            throw new Error(error);
+        });
+        
+    }
+
+    loginUser(email: string,password: string){
+      let Email= email;
+      let Password= password;
+      console.log(email+'/'+password);
+      return firebase.auth().signInWithEmailAndPassword(email, password);
+      
+    }
+  }
+
